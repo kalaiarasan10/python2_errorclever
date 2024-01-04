b36f4c7d2dead1b232e3bfcadd9a096908493eb9
kalaiarasan10 <Kalai@essentials.selfmade.ninja>
2024-01-04 14:11:06 +0000

    new file modified

---
+++ python_example.py
@@ -0,0 +1,15 @@
+a = int(input("enter the number from  even :- "))
+b = int(input("enter the number end of even number:- "))
+# k = 0
+
+# for i in range(a,b+1):
+#     if (i%2 == 0):
+#         k = k+1
+#         # print(i)
+# print("k value is:-",k)
+    
+print("printing natural numbers")
+
+# for i in range (a,b+1):
+    
+    
